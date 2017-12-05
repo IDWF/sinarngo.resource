373eb5185470249f1d07e0a898f806f6cd8ade8a
holdenhao <holdenhao@gmail.com>
2017-12-05 23:17:03 +0800

    make vocabulary translatable

--- sinarngo/resource/vocabulary/types.py
+++ sinarngo/resource/vocabulary/types.py
@@ -5,50 +5,61 @@ from zope.schema.interfaces import IVocabularyFactory
 from zope.component import getUtility
 from z3c.formwidget.query.interfaces import IQuerySource
 
+from sinarngo.resource import MessageFactory as _
+
 class Types(grok.GlobalUtility):
     grok.name('sinarngo.resource.types')
     grok.implements(IVocabularyFactory)
 
     _terms = [
-        dict(   value = 'activity',
-            title = 'Activity or Event Report'),
-        dict(   value = 'application',
-            title = 'Application or Product'),
-        dict(   value = 'books',
-            title = 'Books'),
-        dict(   value = 'promotional',
-            title = 'Brochure, Promotional Materials'),
+        dict(
+            value = 'activity',
+            title = _(u'Activity or Event Report')
+            ),
+        dict(
+            value = 'application',
+            title = _(u'Application or Product')
+            ),
+        dict(
+            value = 'books',
+            title = _(u'Books')
+            ),
+        dict(
+            value = 'promotional',
+            title = _(u'Brochure, Promotional Materials')
+            ),
         dict(
             value = 'data',
-            title = u'Data, Surveys, Fact Sheets'
+            title = _(u'Data, Surveys, Fact Sheets')
             ),
         dict( 
             value = 'legislation',
-            title = u'Legislation, Regulations',
-        ),
+            title = _(u'Legislation, Regulations')
+            ),
         dict( 
             value = 'media',
-            title = u'Interview, Panel, Presentation',
-        ),
+            title = _(u'Interview, Panel, Presentation')
+            ),
         dict(
             value = 'periodical',
-            title = 'Newsletter, Journal',
-        ),
-        dict( value = 'policy',
-          title = u'Policy, Strategy or Plan',
-        ),
+            title = _(u'Newsletter, Journal')
+            ),
+        dict( 
+            value = 'policy',
+            title = _(u'Policy, Strategy or Plan')
+            ),
         dict(
              value = 'project',
-             title = u'Project',
-         ),
+             title = _(u'Project')
+            ),
         dict(
              value = 'research',
-             title = u'Research reports, working paper',
-         ),
+             title = _(u'Research reports, working paper')
+            ),
         dict(
             value = 'training',
-            title = u'Training Material, Guides, Organizing/Educational Materials'
-        ),
+            title = _(u'Training Material, Guides, Organizing/Educational Materials')
+            ),
             ]
 
     def __call__(self, context):
